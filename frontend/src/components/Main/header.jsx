f71c3b6665279d3577316cd914cf404d5ff3afc5
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-10-11 18:19:52 +0300

    add ErrorPopup component to header component

--- frontend/src/components/Main/header.jsx
+++ frontend/src/components/Main/header.jsx
@@ -7,13 +7,14 @@ import { styled } from '@mui/material/styles';
 import logoCroped from '../photos/logo croped.png'
 import LoginBtn from '../Essential/login'
 import LogoutBtn from '../Essential/logout'
-
-
+import ErrorPopup from '../Essential/ErrorPopup.jsx';
+require('../../utilities/console.js');
 
 const Header = ({user,show,currentUser,userPayingSystem,isAdmin, redirect_url}) =>{
     
     
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [openError, setOpenError] = React.useState(false);
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -101,87 +102,97 @@ const Header = ({user,show,currentUser,userPayingSystem,isAdmin, redirect_url}) 
         }
     ]
     return(
-        <header>
-            <img id='logo' src={logoCroped} alt='logo' />
-            {/* <h1>essential change</h1> */}
-            {
-                redirect_url === 'http://localhost:3000/' ?
+        <>
+            <header>
+                <img id='logo' src={logoCroped} alt='logo' />
+                {
+                    redirect_url === 'http://localhost:3000/' ?
                     <h1>change redirect url</h1>
-                :
+                    :
                     ''
-            }
-            
-            <ul className='nav-bar'>
-                {navber_name.map((obj) =>{
-                    if(obj.id ===5){
-                        if(currentUser.name !=='' || user){
-                            return(  
-                                <div>
-                                    <Button
-                                        id="fade-button"
-                                        aria-controls={open ? 'fade-menu' : undefined}
-                                        aria-haspopup="true"
-                                        aria-expanded={open ? 'true' : undefined}
-                                        onClick={handleClick}
-                                        className='dropdown-span'
-                                    >
-                                        حسابي
-                                    </Button>
-                                    <StyledMenu
-                                        id="demo-customized-menu"
-                                        MenuListProps={{
-                                        'aria-labelledby': 'demo-customized-button',
-                                        }}
-                                        anchorEl={anchorEl}
-                                        open={open}
-                                        onClose={handleClose}
-                                    >
-                                        <MenuItem onClick={handleClose}>
-                                            <div key={'dropdown-menu-info'} className='dropdown-menu-info'>
-                                                <img src={currentUser.picture} alt='profile'/>
-                                                <h2 className='dropdownitem-user-name' key={'dropdonwitem'}>{currentUser.name}</h2>
-                                            </div>
-                                        </MenuItem>
-                                        <MenuItem onClick={handleClose}>
-                                            <p className={'dropdonwitem'} key={'dropdonwitem'}>{
-                                                userPayingSystem === undefined ||  userPayingSystem ==='none' ||  userPayingSystem ===null || userPayingSystem.length === 0?
-                                                'غير محدد'
-                                                :
-                                                userPayingSystem + ':نوع الاشتراك'
+                }
+                
+                <ul className='nav-bar'>
+                    {navber_name.map((obj) =>{
+                        if(obj.id ===5){
+                            if(currentUser.name !=='' || user){
+                                return(  
+                                    <div>
+                                        <Button
+                                            id="fade-button"
+                                            aria-controls={open ? 'fade-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={open ? 'true' : undefined}
+                                            onClick={handleClick}
+                                            className='dropdown-span'
+                                        >
+                                            حسابي
+                                        </Button>
+                                        <StyledMenu
+                                            id="demo-customized-menu"
+                                            MenuListProps={{
+                                                'aria-labelledby': 'demo-customized-button',
+                                            }}
+                                            anchorEl={anchorEl}
+                                            open={open}
+                                            onClose={handleClose}
+                                            >
+                                            <MenuItem onClick={handleClose}>
+                                                <div key={'dropdown-menu-info'} className='dropdown-menu-info'>
+                                                    <img src={currentUser.picture} alt='profile'/>
+                                                    <h2 className='dropdownitem-user-name' key={'dropdonwitem'}>{currentUser.name}</h2>
+                                                </div>
+                                            </MenuItem>
+                                            <MenuItem onClick={handleClose}>
+                                                <p className={'dropdonwitem'} key={'dropdonwitem'}>{
+                                                    userPayingSystem === undefined ||  userPayingSystem ==='none' ||  userPayingSystem ===null || userPayingSystem.length === 0?
+                                                    'غير محدد'
+                                                    :
+                                                    userPayingSystem + ':نوع الاشتراك'
                                                 }</p>
-                                        </MenuItem>
-                                        <MenuItem onClick={handleClose}><LogoutBtn redirect_url={redirect_url}/></MenuItem>
-                                    </StyledMenu>
-                                </div> 
-                            )
-                        }else{
-                            return(<LoginBtn />)   
-                        }
-                    }else{
-                            if(obj.EngName ==='#main'){
-                                return(<li className={'item'+obj.id} key={'item'+obj.id}>
-                                <Link to={'/Hany-AbdulGawed-platform/'}>{obj.arbicName}</Link>
-                                </li>)
-                            }
-                            if(obj.EngName ==='#payingsystem'){
-                                return(<li className={'item'+obj.id} key={'item'+obj.id}>
-                                <Link to={'/Hany-AbdulGawed-platform/PayingSystem'}>{obj.arbicName}</Link>
-                                </li>)
-                            }
-                            if(obj.EngName ==='#admin'){
-                                return(
-                                    isAdmin ?
-                                        <li className={'item'+obj.id} key={'item'+obj.id}>
-                                        <Link to={'/dashboard'}>{obj.arbicName}</Link>
-                                        </li>
-                                    :
-                                        ''
+                                            </MenuItem>
+                                            <MenuItem >
+                                                    <button onClick={() => {setOpenError(true); setAnchorEl(null);}}>
+                                                        الأبلاغ عن مشكلة
+                                                    </button>
+                                            </MenuItem>
+                                            <MenuItem onClick={handleClose}><LogoutBtn redirect_url={redirect_url}/></MenuItem>
+                                        </StyledMenu>
+                                    </div> 
                                 )
+                            }else{
+                                return(<LoginBtn />)   
                             }
-                    }
-                })}
-            </ul>
-        </header>
+                        }else{
+                                if(obj.EngName ==='#main'){
+                                    return(<li className={'item'+obj.id} key={'item'+obj.id}>
+                                    <Link to={'/Hany-AbdulGawed-platform/'}>{obj.arbicName}</Link>
+                                    </li>)
+                                }
+                                if(obj.EngName ==='#payingsystem'){
+                                    return(<li className={'item'+obj.id} key={'item'+obj.id}>
+                                    <Link to={'/Hany-AbdulGawed-platform/PayingSystem'}>{obj.arbicName}</Link>
+                                    </li>)
+                                }
+                                if(obj.EngName ==='#admin'){
+                                    return(
+                                        isAdmin ?
+                                        <li className={'item'+obj.id} key={'item'+obj.id}>
+                                            <Link to={'/dashboard'}>{obj.arbicName}</Link>
+                                            </li>
+                                        :
+                                        ''
+                                    )
+                                }
+                            }
+                        })}
+                </ul>
+            </header>
+            {
+                openError && 
+                <ErrorPopup isShown={openError} setIsShown={setOpenError}/>
+            }
+        </>
     )
 }
 
